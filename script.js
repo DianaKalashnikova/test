582942856c58aa98b29f2238891288b47aacce5b
Diana <dianagolubeva707@gmail.com>
2023-10-25 18:13:57 +0300

    third

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
-let money = prompt('Ваш бюджет на месяц?', ''),
+let money, time;
+
+
+function start() {
+    money = +prompt('Ваш бюджет на месяц?', '');
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+    }
+}
+    start();
+
+
+
 let appData = {
-    budget: "money",
-    timedata: "time",
+    budget: money,
+    timedata: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false    
+    savings: true   
 };
 
-let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    a2 = prompt('Во сколько обойдется?”', ''),
-    a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    a4 = prompt('Во сколько обойдется?', '');
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+            b = prompt('Во сколько обойдется?', '');
+    
+        if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+            && a !='' && b!='' && a.length < 50) {
+            console.log('done');    
+            appData.expenses[a] = b;    
+        } else {
+            i = i - 1;
+        }        
+    };
+}
+chooseExpenses();
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
-alert(appData.budget / 30);
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка'); 
+
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка'); 
+} else if(appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка'); 
+} else {
+    console.log('Произошла ошибка'); 
+}
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с депозита: " + appData.monthIncome)            
+    }
+}
+checkSavings();
